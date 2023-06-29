8fed85f03b5825a5568a463867756c57f474e79e
Phinatech <ugbojudithchinemerem@gmil.com>
2023-06-29 23:04:55 +0100

    starting with hero

--- personal-Project/src/Pages/Home/Hero.tsx
+++ personal-Project/src/Pages/Home/Hero.tsx
@@ -2,8 +2,20 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+    <div className="w-[100%] h-[fit-content] justify-center flex bg-hero bg-no-repeat">
+      <div className=" w-[95%] flex ]">
+        {/* <div className=" "> left1</div> */}
+        <div className="">
+          <div className="text-[#181D4E] font-bold text-[35px] ">Pick the best toy for your kid</div>
+          <div>
+            We offer a premium service,whether you are shopping at one of our
+            flagship store or via our website
+          </div>
+          <div>Discover Now</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero
